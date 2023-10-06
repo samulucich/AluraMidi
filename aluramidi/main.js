571e23beb46b2a38f869871cc1bc4d67f7a95a70
Estudante Alura <110406167+samulucich@users.noreply.github.com>
2023-10-06 08:44:49 -0300

    Add files via upload

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -3,12 +3,14 @@ function tocaSom (idElementoAudio) {
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
+//variavel
 let contador = 0;
 
 //enquanto
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSom;
+    listaDeTeclas[contador].onclick = function() {
+        tocaSom('#som_tecla_pom');
+    }
 
     contador = contador + 1;
 
