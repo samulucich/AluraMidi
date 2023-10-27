9a95c04754cdb65a38dd9b546e77bc3218439553
Estudante Alura <110406167+samulucich@users.noreply.github.com>
2023-10-27 08:43:49 -0300

    Add files via upload

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,5 +1,20 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento === null) {
+      //alert('Elemento não encontrado');
+      console.log('Elemento não encontrado');
+    }
+
+    if(elemento != null) {
+
+      if(elemento.localName === 'audio'){
+        elemento.play();
+
+      }
+
+      
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -12,6 +27,20 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     tecla.onclick = function() {
       tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+       console.log(evento.code =='Space')
+    
+       if(evento.code === 'Space' || evento.code === 'Enter'){
+          tecla.classList.add('ativa');
+       }
+
+    }
+
+    tecla.onkeyup = function() {
+      tecla.classList.remove('ativa');
     }
 
 }
