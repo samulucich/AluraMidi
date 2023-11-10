51b716f5baf2a41f9fdc3d303e95b54e8f33a286
Estudante Alura <110406167+samulucich@users.noreply.github.com>
2023-11-10 08:00:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento === null) {
+      //alert('Elemento não encontrado');
+      console.log('Elemento não encontrado');
+    }
+
+    if(elemento != null) {
+
+      if(elemento.localName === 'audio'){
+        elemento.play();
+
+      }
+
+      
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function() {
+      tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+       console.log(evento.code =='Space')
+    
+       if(evento.code === 'Space' || evento.code === 'Enter'){
+          tecla.classList.add('ativa');
+       }
+
+    }
+
+    tecla.onkeyup = function() {
+      tecla.classList.remove('ativa');
+    }
+
+}
